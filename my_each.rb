8cc1334d20ae2d8d6f149cc3430fcf465b6b6f81
s1l3ntq <qimiller89@gmail.com>
2020-11-17 01:31:30 -0600

    commit tree d579e5eb95a9d2c96be089defc99f6f62cc606e0

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
+  q = 0
+  while q < array.length 
+    yield(array[q])
+    q = q + 1
+  end
+  array
   # code here
 end
